c32aa8c8f76e848b953f3fa53c26679f86465cde
14islands <14islands@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-08 12:29:10 +0000

    Tagging version 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/responsiveio/tags/1.0.5@889375 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/responsive-io.php
@@ -0,0 +1,58 @@
+<?php
+/*
+@package   Responsive_IO
+@author    14islands <hello@14islands.com>
+@license   GPL-2.0+
+@link      http://responsive.io
+@copyright 2013 14islands
+@wordpress-plugin
+Version:           1.0.5
+Plugin Name:       responsive.io
+Plugin URI:        http://responsive.io/
+Description:       This plugin optimizes and resizes all images in your content using the responsive.io service. Sign up for a free 30 day trial at http://responsive.io.
+Author:            14islands
+Author URI:        http://14islands.com
+Text Domain:       responsive-io-locale
+License:           GPL-2.0+
+License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+Domain Path:       /languages
+GitHub Plugin URI: https://github.com/14islands/wp-reponsive-io
+GitHub Branch:     master
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-responsive-io.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ *
+ * - the name of the class defined in
+ *   `class-responsive-io.php`
+ */
+register_activation_hook( __FILE__, array( 'Responsive_IO', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Responsive_IO', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'Responsive_IO', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * The code below is intended to to give the lightest footprint possible.
+ */
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-responsive-io-admin.php' );
+	add_action( 'plugins_loaded', array( 'Responsive_IO_Admin', 'get_instance' ) );
+
+}
